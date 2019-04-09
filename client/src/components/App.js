f6aa13a6d1b3737e54f828bd9ba1c5f51550ae17
Joy <joy.js.mun@gmail.com>
2019-04-10 01:05:35 +0900

    ADD: react-router-dom setting

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import {BrowserRouter,route} from 'react-router-dom';
 import Header from './Header';
+import StreamList from './streams/StreamList';
+import StreamCreate from './streams/StreamCreate';
 
 const App = () => {
     return(
-        <div>App</div>
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <route path="/" component={StreamList} />
+                    <route path="/streams/new" component={StreamCreate} />
+                </div>
+            </BrowserRouter>
+        </div>
     )
 }
 
