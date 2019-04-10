3257533e5b17b0c2cbb0fc773cab391918a8ef51
Joy <joy.js.mun@gmail.com>
2019-04-11 02:57:05 +0900

    ADD: completed validation form

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -3,12 +3,24 @@ import {Field,reduxForm} from 'redux-form';
 
 class StreamCreate extends React.Component{
 
-    renderInput({input,label,meta}){
+    renderError({error,touched}){
+        if(error && touched){
+            return(
+                <div className="ui error message">
+                    <div className="header">{error}</div>
+                </div>
+            )
+        }
+    }
+
+    renderInput = ({input,label,meta}) =>{
+        
+        const className=`field ${meta.error && meta.touched ? 'error' : ''}`;
         return (
-            <div className="field">
+            <div className={className}>
                 <label>{label}</label>
                 <input {...input} autoComplete="off"/>
-                <div>{meta.error}</div>
+                {this.renderError(meta)}
             </div>
         )
     }
@@ -19,7 +31,7 @@ class StreamCreate extends React.Component{
 
     render(){
         return(
-           <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+           <form className="ui form error" onSubmit={this.props.handleSubmit(this.onSubmit)}>
                <Field name="title" component={this.renderInput} label="Enter the title"/>
                <Field name="description" component={this.renderInput} label="Enter the description"/>
                <button className="ui button primary">Submit</button>
