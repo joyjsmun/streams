ab722f3a7fc49761d96dd93097cbe063de4de4b6
Joy <joy.js.mun@gmail.com>
2019-04-13 04:13:04 +0900

    ADD: create button

--- client/src/action/index.js
+++ client/src/action/index.js
@@ -25,7 +25,8 @@ export const signOut = () => {
 
 export const createStream = (formValues) => async (dispatch,getState) => { 
     const {userId} = getState().auth;
-   const response = await streams.post('/streams',{...formValues, userId});
+    const {isSignedIn} = getState().auth;
+   const response = await streams.post('/streams',{...formValues, userId,isSignedIn});
 
     dispatch({type:CREATE_STREAM, payload: response.data})
 };
--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {fetchStreams} from '../../action';
 import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 
 class StreamList extends React.Component{
 
@@ -34,6 +35,17 @@ class StreamList extends React.Component{
             )
         })
     }
+
+    renderCreate(){
+        console.log(this.props)
+        if(this.props.isSignedIn){
+            return(
+                <div style={{textAlign:'right'}}>
+                    <Link to="/streams/new" className="ui button primary">Create Stream</Link>
+                </div>
+            )
+        }
+    }
     render(){
         return(
             <div>
@@ -41,6 +53,7 @@ class StreamList extends React.Component{
                 <div className="ui celled list">
                 {this.renderList()}
                 </div>
+                {this.renderCreate()}
             </div>
         )
     }
@@ -49,7 +62,8 @@ class StreamList extends React.Component{
 const mapStateToProps = state => {
     return {
         streams: Object.values(state.streams),
-        currentUserId: state.auth.userId
+        currentUserId: state.auth.userId,
+        isSignedIn: state.auth.isSignedIn
     }
 }
 
