c3342104625d9ab262d13df31f44dbbb8de7b5e9
Joy <joy.js.mun@gmail.com>
2019-04-13 02:26:14 +0900

    ADD: connect streamList component w/ fetchStreams

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import {fetchStreams} from '../../action';
+import {connect} from 'react-redux';
 
 class StreamList extends React.Component{
+
+    componentDidMount(){
+        this.props.fetchStreams();
+    }
+
+
+    renderList(){
+        return this.props.streams.map(stream => {
+            return(
+                <div className="item" key={stream.id}>
+                    <i className="large middle aligned icon camera"/>
+                    <div className="content">
+                    {stream.title}
+                    </div>
+                    <div className="description">{stream.descripton}</div>
+                </div>
+            )
+        })
+    }
     render(){
         return(
-            <div>StreamList</div>
+            <div>
+                <h2>Streams</h2>
+                <div className="ui celled list">
+                {this.renderList()}
+                </div>
+            </div>
         )
     }
 }
 
-export default StreamList;
+const mapStateToProps = state => {
+    return {streams: Object.values(state.streams)}
+}
+
+export default connect(mapStateToProps,{fetchStreams})(StreamList);
